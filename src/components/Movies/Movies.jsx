cdcd85aaba3f32f1544dd238af490c45df2fb2b0
Saad <mu.saadafzal@gmail.com>
2022-06-07 20:58:28 +0100

    Movies Initial rendering completed

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -6,14 +6,41 @@ import {
   Typography,
 } from "@mui/material";
 import { useSelector } from "react-redux";
-
 import { useGetMoviesQuery } from "../../services/TMDB";
+import { MovieList } from "../";
 
 const Movies = () => {
-  const { data } = useGetMoviesQuery();
-  console.log(data);
+  const { data, error, isFetching } = useGetMoviesQuery();
 
-  return <div>Movies</div>;
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
+
+  if (!data.results.length) {
+    return (
+      <Box display="flex" alignItems="center" mt="2opx">
+        <Typography variant="h4">
+          No Movies that match that Name.
+          <br />
+          Please Search for something else.
+        </Typography>
+      </Box>
+    );
+  }
+
+  if (error) {
+    return "An error has occurred";
+  }
+
+  return (
+    <div>
+      <MovieList movies={data} />
+    </div>
+  );
 };
 
 export default Movies;
